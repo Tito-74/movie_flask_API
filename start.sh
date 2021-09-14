35602ce5ffcfc0a25c971986070f920543c83eaf
Tito-74 <kipkirui133@gmail.com>
2021-09-14 15:10:08 +0300

    initial commit

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY=ac371c8afc33b37cb001f0ce7c15efc6
+export SECRET_KEY=Flask WTF Secret Key
+# MOVIE_API_KEY = 'ac371c8afc33b37cb001f0ce7c15efc6'
+# SECRET_KEY = 'Flask WTF Secret Key'
+
+python3 manage.py server
